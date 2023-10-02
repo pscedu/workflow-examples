0eb49e02acc2a93772a16061b57435e47e2a6d80
icaoberg <icaoberg@andrew.cmu.edu>
2023-10-02 14:17:50 -0400

    Added Exercise 4

---
+++ singularity/exercise4/generate_binaries.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+TOOLS=(magick
+magick
+animate
+compare
+composite
+conjure
+convert
+display
+identify
+mogrify
+montage)
+
+cat << EOF > template
+#!/bin/bash
+
+VERSION=7.1.1-15
+PACKAGE=imagemagick
+TOOL=TOOL_NAME
+DIRECTORY=\$(dirname \$0)
+
+STORAGES=(/ocean /bil /hive /local)
+
+OPTIONS=""
+for STORAGE in "\${STORAGES[@]}"
+do
+  	if [ -d "\$STORAGE" ]; then
+                OPTIONS=\$OPTIONS" -B \$STORAGE"
+        fi
+done
+
+singularity exec \$OPTIONS \$DIRECTORY/singularity-\$PACKAGE-\$VERSION.sif \$TOOL "\$@"
+EOF
+
+for TOOL in "${TOOLS[@]}"
+do
+	echo "* "$TOOL
+        cp template $TOOL
+	sed -i "s/TOOL_NAME/$TOOL/g" $TOOL
+	chmod +x $TOOL
+done
+
+rm -f template
