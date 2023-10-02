bdf884117458c6cb757c701bc2f73e87982a5016
Ivan Cao-Berg <icaoberg@andrew.cmu.edu>
2023-10-02 15:12:08 -0400

    Updated script

--- nextflow/install.sh
+++ nextflow/install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sdk install java 17.0.6-amzn
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install java 17.0.6-amzn
+
 if [ ! -d ~/bin ]; then mkdir ~/bin; fi
 cd ~/bin && curl -s https://get.nextflow.io | bash
 chmod +x ~/bin/nextflow
