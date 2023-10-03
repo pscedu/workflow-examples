9eb965949d8881f901c134d7d9f2cf0e6d698dbe
icaoberg <icaoberg@andrew.cmu.edu>
2023-10-03 12:09:29 -0400

    Added example that converts an OME.TIFF into an N5 file

--- cwl/bioformats2raw/script.sh
+++ cwl/bioformats2raw/script.sh
@@ -4,5 +4,6 @@
 #SBATCH -n 5
 #SBATCH --mem=10000M
 
+module load anaconda3
 wget -nc https://downloads.openmicroscopy.org/images/OME-TIFF/2016-06/BBBC/NIRHTa-001.ome.tiff
 cwl-runner --singularity bioformats2raw.cwl input.yaml
