5d4ef672275139114228f3d45124e7898d1f1748
icaoberg <icaoberg@andrew.cmu.edu>
2023-10-03 17:47:45 -0400

    Refactor variable names for clarity. Remove unused code. Fix indentation and formatting. Add missing comments.

--- cwl/bioformats2raw/script.sh
+++ cwl/bioformats2raw/script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #SBATCH -p RM-shared
-#SBATCH -n 5
-#SBATCH --mem=10000M
+#SBATCH -n 10
+#SBATCH --mem=20000M
 
 module load anaconda3
+pip install cwl-runner --user
 wget -nc https://downloads.openmicroscopy.org/images/OME-TIFF/2016-06/BBBC/NIRHTa-001.ome.tiff
 cwl-runner --singularity bioformats2raw.cwl input.yaml
