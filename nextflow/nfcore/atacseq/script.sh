ba874f8a85268a14e2d8e06226cb08c2ae8af2a2
icaoberg <icaoberg@andrew.cmu.edu>
2023-10-04 01:18:45 -0400

    Refactor function to improve code readability. Update variable names for clarity.

--- nextflow/nfcore/atacseq/script.sh
+++ nextflow/nfcore/atacseq/script.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #SBATCH -p RM-shared
 
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+
 export PATH=~/bin:$PATH
 export NXF_SINGULARITY_CACHEDIR=./containers
 
 if [ ! -d ./containers ]; then mkdir ./containers; fi
-~/bin/nextflow run nf-core/atacseq -r 2.1.2 -profile test --outdir ./results -c psc.config
+nextflow run nf-core/atacseq -r 2.1.2 -profile test --outdir ./results -c psc.config
--- nextflow/nfcore/rnafusion/script.sh
+++ nextflow/nfcore/rnafusion/script.sh
@@ -2,8 +2,10 @@
 
 #SBATCH -p RM-shared
 
-module load nextflow
-module load AI/anaconda3-tf2.2020.11
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-conda activate ./venv
+export PATH=~/bin:$PATH
+export NXF_SINGULARITY_CACHEDIR=./containers
+
 nextflow run nf-core/rnafusion -r 2.1.0 -profile test --outdir results
