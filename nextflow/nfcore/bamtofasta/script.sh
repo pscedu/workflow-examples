40ad7d14d677d3f1ccf5673b049b4e0124e3b0f9
icaoberg <icaoberg@andrew.cmu.edu>
2023-10-04 01:54:09 -0400

    Added bamtofasta

---
+++ nextflow/nfcore/bamtofasta/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -p RM-shared
+#SBATCH -n 10
+#SBATCH --mem=20000M
+
+module load anaconda3
+module load FastQC
+module load samtools
+
+pip install multiqc --user -q
+
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+export PATH=~/bin:$PATH
+export NXF_SINGULARITY_CACHEDIR=./containers
+
+if [ ! -d ./containers ]; then mkdir ./containers; fi
+nextflow run nf-core/bamtofastq -r 2.0.0 -profile test --outdir ./results
