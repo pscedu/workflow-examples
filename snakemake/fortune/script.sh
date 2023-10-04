4f7497c878cebfa7e74672a689463dfa960662c7
icaoberg <icaoberg@andrew.cmu.edu>
2023-10-03 21:10:48 -0400

    Added fortune example

---
+++ snakemake/fortune/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export PATH=$PATH:~/.local/bin
+
+wget -nc https://raw.githubusercontent.com/ruanyf/fortunes/master/data/fortunes
+
+pip install fortune --user -q
+pip install snakemake --user -q
+
+snakemake --cores 1
+
+if [ -f fortunes ]; then
+	rm -f fortunes
+fi
